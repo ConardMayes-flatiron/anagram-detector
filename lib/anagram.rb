cbe735ee8dfe3bc92859d8a2c7caae07f3f25dc9
ConardMayes-flatiron <ConradAsatoMayes+flatironschool@gmail.com>
2020-11-28 17:51:15 -0600

    commit tree 4b50e903efadbc04bcc31e79c844fa75dfe39869

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,22 @@
 # Your code goes here!
+class Anagram
+    attr_accessor :word
+
+    def initialize(word)
+        @word = word
+    end
+
+    def match(submitted)
+        @final_array=[]
+        submitted.each do |test_word|
+            
+            
+            if (test_word.split("")).sort == (@word.split("")).sort
+                @final_array << test_word
+            end
+        end
+
+        @final_array
+        
+    end
+end
